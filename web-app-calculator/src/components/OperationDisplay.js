bd63d6e7601a53500561d8af967f0f842edff389
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-11 10:41:11 +1200

    fixing comments for clarity

--- web-app-calculator/src/components/OperationDisplay.js
+++ web-app-calculator/src/components/OperationDisplay.js
@@ -3,10 +3,13 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-// OperationDisplay should have its own css styling instead of being mixed with the main code
+/* OperationDisplay should have its own css styling instead of being mixed 
+with the main code*/
 
-// I would recommend renaming the function OperationDisplay to calculatorDisplay or similar to make it more clear what this function is doing.
-// recommend defining the props of what h and d stand for more clarity on what data these are likely to hold
+/* I would recommend renaming the function OperationDisplay to calculatorDisplay 
+or similar to make it more clear what this function is doing. Also recommend defining 
+the props of what h and d stand for more clarity on what data these are likely to hold */
+
 export default function OperationDisplay({ h, d }) {
   return (
     <>
@@ -17,7 +20,7 @@ export default function OperationDisplay({ h, d }) {
           <Typography>Past Calculations: </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          {/* remove the ? */}
+          {/* remove the ? and clearly define the h */}
           {h?.map((operation, index) => (
             <Typography key={index}>{operation}</Typography>
           ))}
