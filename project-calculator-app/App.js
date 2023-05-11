f38ef68ebbac2f5cfe90849c9a35177eae7d5ab3
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-11 18:28:09 +1200

    adding the buttons component

--- project-calculator-app/App.js
+++ project-calculator-app/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Buttons from "./components/ButtonContainer";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Buttons />
       <Text>Open up App.js to start working on your app! hello</Text>
       <StatusBar style="auto" />
     </View>
