3e23c109d3dc96cb46863074ef87c9129133c052
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-12 14:58:03 +1200

    added styling for buttons and zero

--- project-calculator-app/components/ButtonContainer.js
+++ project-calculator-app/components/ButtonContainer.js
@@ -1,6 +1,18 @@
 import { Pressable, Text, StyleSheet } from "react-native";
 
 const buttons = ({ value, onButtonPress }) => {
+  if (value === "0") {
+    console.log("zero " + value);
+    return (
+      <Pressable
+        style={[styles.buttons, styles.zeroButton]}
+        onPress={onButtonPress}
+      >
+        <Text style={styles.zero}>{value}</Text>
+      </Pressable>
+    );
+  }
+
   console.log("buttons " + value);
   return (
     <Pressable style={styles.buttons} onPress={onButtonPress}>
@@ -12,15 +24,26 @@ export default buttons;
 
 const styles = StyleSheet.create({
   buttons: {
+    flexBasis: "32%",
+    display: "flex",
     color: "#faf7f8",
+    borderRadius: 20,
     backgroundColor: "#2a2829",
-    height: 50,
-    width: 50,
     justifyContent: "center",
     alignItems: "center",
+    margin: 2,
+    padding: 20,
   },
   value: {
     color: "#f4926a",
+    fontSize: 30,
+  },
+  zeroButton: {
+    flexGrow: 1,
+    flexBasis: "50%",
+  },
+  zero: {
+    color: "#eb4034",
     fontSize: 30,
   },
 });
