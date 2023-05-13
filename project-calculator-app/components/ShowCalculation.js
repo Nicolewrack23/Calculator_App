b55bb8a72f39746a9d540d388d566d6c8f48bf6f
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-13 14:22:52 +1200

    matching design to wireframe

--- project-calculator-app/components/ShowCalculation.js
+++ project-calculator-app/components/ShowCalculation.js
@@ -1,12 +1,39 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const ShowCalculation = ({ buttonClicked }) => {
   console.log(buttonClicked);
 
   return (
-    <View>
-      <Text>hello {buttonClicked}</Text>
-    </View>
+    <>
+      <View style={styles.row1}>
+        <Text style={styles.calculation}>{buttonClicked}</Text>
+        <Text style={[styles.working]}>{buttonClicked}</Text>
+      </View>
+    </>
   );
 };
 export default ShowCalculation;
+
+const styles = StyleSheet.create({
+  row1: {
+    justifyContent: "end",
+    alignItems: "center",
+    width: "100%",
+  },
+  calculation: {
+    borderBottomColor: "#BA9D9F",
+    borderBottomWidth: 3,
+    paddingVertical: 10,
+    width: "90%",
+    fontSize: 32,
+    textAlign: "right",
+    color: "grey",
+  },
+  working: {
+    paddingVertical: 10,
+    width: "90%",
+    fontSize: 64,
+    textAlign: "right",
+    color: "#fff",
+  },
+});
