ccdc6e8dce5e36a268971bebf538dc96784ab833
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-15 14:35:13 +1200

    adding numbers onto each other checking operator

--- project-calculator-app/components/Calculate.js
+++ project-calculator-app/components/Calculate.js
@@ -1,4 +1,24 @@
+let buttons = "";
+let operator = null;
 const calculateResult = (buttonValue) => {
-  console.log(buttonValue);
+  if (isOperator(buttonValue)) {
+    if (operator) {
+      return;
+    }
+    operator = buttonValue;
+  }
+
+  buttons = buttons + buttonValue;
+  console.log(buttons);
 };
+
+const isOperator = (buttonValue) => {
+  return (
+    buttonValue === "+" ||
+    buttonValue === "-" ||
+    buttonValue === "X" ||
+    buttonValue === "/"
+  );
+};
+
 export default calculateResult;
