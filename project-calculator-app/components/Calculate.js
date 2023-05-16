c55c5a447f1ac44bb115a2b224e1134b53978bcc
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-17 11:44:55 +1200

    returning an array with equation and result

--- project-calculator-app/components/Calculate.js
+++ project-calculator-app/components/Calculate.js
@@ -4,39 +4,37 @@ let result;
 let equation;
 
 const calculateResult = (firstOperand, operator, secondOperand) => {
-  console.log(`${firstOperand}${operator}${secondOperand}`);
-
   switch (operator) {
     case "+":
       firstNumber = Number(firstOperand);
       secondNumber = Number(secondOperand);
-      result = firstNumber + secondNumber;
+      result = +(firstNumber + secondNumber).toFixed(2);
       equation = `${firstOperand}${operator}${secondOperand}=${result}`;
       console.log("Result calculate +: " + equation);
-      return equation;
+      return [equation, result];
     case "-":
       firstNumber = Number(firstOperand);
       secondNumber = Number(secondOperand);
-      result = firstNumber - secondNumber;
+      result = +(firstNumber - secondNumber).toFixed(2);
       equation = `${firstOperand}${operator}${secondOperand}=${result}`;
       console.log("Result calculate -: " + equation);
-      return equation;
+      return [equation, result];
 
     case "X":
       firstNumber = Number(firstOperand);
       secondNumber = Number(secondOperand);
-      result = firstNumber * secondNumber;
+      result = +(firstNumber * secondNumber).toFixed(2);
       equation = `${firstOperand}${operator}${secondOperand}=${result}`;
       console.log("Result calculate *: " + equation);
-      return equation;
+      return [equation, result];
 
     case "/":
       firstNumber = Number(firstOperand);
       secondNumber = Number(secondOperand);
-      result = firstNumber / secondNumber;
+      result = +(firstNumber / secondNumber).toFixed(2);
       equation = `${firstOperand}${operator}${secondOperand}=${result}`;
       console.log("Result calculate /: " + equation);
-      return equation;
+      return [equation, result];
     default:
       console.log(`Sorry, wrong operator: ${operator}.`);
   }
