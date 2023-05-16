6a733fef8b5a41de2fbbee2450cac85593340b22
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-16 21:35:03 +1200

    code added to calculate the result

--- project-calculator-app/components/Calculate.js
+++ project-calculator-app/components/Calculate.js
@@ -1,6 +1,8 @@
 let buttons = "";
 let operator = null;
-let firstNumber = null;
+// let firstPeriod = false;
+// let secondPeriod = false;
+let firstNumber = "";
 let secondNumber = "";
 let result = "";
 let firstOperand;
@@ -8,33 +10,50 @@ let secondOperand;
 
 const calculateNumbers = (buttonValue) => {
   if (isOperator(buttonValue)) {
-    if (operator) {
+    if (operator || buttonValue === "=") {
       return;
     }
+    // firstPeriod = false;
     firstNumber = buttons;
     operator = buttonValue;
-    console.log("firstNumber: " + firstNumber);
   } else if (operator && !isOperator(buttonValue) && buttonValue !== "=") {
+    // if (buttonValue === "." && secondPeriod) {
+    //   return;
+    // }
+    // if (buttonValue === ".") {
+    //   secondPeriod = true;
+    // }
     secondNumber = secondNumber + buttonValue;
-    console.log("secondNumber: " + secondNumber);
   }
+  // if (buttonValue === "." && firstPeriod) {
+  //   console.log("debug" + firstPeriod);
+  //   return;
+  // }
+  // if (buttonValue === "." && !firstPeriod) {
+  //   firstPeriod = true;
+  // }
+
   if (buttonValue !== "=") {
+    ShowCalculation();
     buttons = buttons + buttonValue;
+
+    // console.log(buttons);
   } else {
     calculateResult();
     buttons = "";
     operator = null;
     firstNumber = null;
     secondNumber = "";
+    // firstPeriod = false;
   }
 };
 
 const calculateResult = () => {
   switch (operator) {
     case "+":
-      firstOperand = Number(firstNumber);
-      secondOperand = Number(secondNumber);
-      result = firstOperand + secondOperand;
+      firstNumber = Number(firstNumber);
+      secondNumber = Number(secondNumber);
+      result = firstNumber + secondOperand;
       console.log("Result: " + result);
       break;
     case "-":
@@ -58,6 +77,9 @@ const calculateResult = () => {
     default:
       console.log(`Sorry, wrong operator: ${operator}.`);
   }
+};
+const ShowCalculation = () => {
+  console.log(`ShowCalculation ${firstNumber}${operator}${secondNumber}=`);
 };
 
 const isOperator = (buttonValue) => {
