b3e2ff0e953d1bee765a13d5f928c2ec4e8819f8
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-17 10:55:47 +1200

    calculate Result depending on operator

--- project-calculator-app/components/Calculate.js
+++ project-calculator-app/components/Calculate.js
@@ -1,94 +1,45 @@
-let buttons = "";
-let operator = null;
-// let firstPeriod = false;
-// let secondPeriod = false;
-let firstNumber = "";
-let secondNumber = "";
-let result = "";
-let firstOperand;
-let secondOperand;
+let firstNumber;
+let secondNumber;
+let result;
+let equation;
 
-const calculateNumbers = (buttonValue) => {
-  if (isOperator(buttonValue)) {
-    if (operator || buttonValue === "=") {
-      return;
-    }
-    // firstPeriod = false;
-    firstNumber = buttons;
-    operator = buttonValue;
-  } else if (operator && !isOperator(buttonValue) && buttonValue !== "=") {
-    // if (buttonValue === "." && secondPeriod) {
-    //   return;
-    // }
-    // if (buttonValue === ".") {
-    //   secondPeriod = true;
-    // }
-    secondNumber = secondNumber + buttonValue;
-  }
-  // if (buttonValue === "." && firstPeriod) {
-  //   console.log("debug" + firstPeriod);
-  //   return;
-  // }
-  // if (buttonValue === "." && !firstPeriod) {
-  //   firstPeriod = true;
-  // }
+const calculateResult = (firstOperand, operator, secondOperand) => {
+  console.log(`${firstOperand}${operator}${secondOperand}`);
 
-  if (buttonValue !== "=") {
-    ShowCalculation();
-    buttons = buttons + buttonValue;
-
-    // console.log(buttons);
-  } else {
-    calculateResult();
-    buttons = "";
-    operator = null;
-    firstNumber = null;
-    secondNumber = "";
-    // firstPeriod = false;
-  }
-};
-
-const calculateResult = () => {
   switch (operator) {
     case "+":
-      firstNumber = Number(firstNumber);
-      secondNumber = Number(secondNumber);
-      result = firstNumber + secondOperand;
-      console.log("Result: " + result);
-      break;
+      firstNumber = Number(firstOperand);
+      secondNumber = Number(secondOperand);
+      result = firstNumber + secondNumber;
+      equation = `${firstOperand}${operator}${secondOperand}=${result} `;
+      console.log("Result calculate +: " + equation);
+      return equation;
     case "-":
-      firstOperand = Number(firstNumber);
-      secondOperand = Number(secondNumber);
-      result = firstOperand - secondOperand;
-      console.log("Result: " + result);
-      break;
+      firstNumber = Number(firstOperand);
+      secondNumber = Number(secondOperand);
+      result = firstNumber - secondNumber;
+      equation = `${firstOperand}${operator}${secondOperand}=${result} `;
+      console.log("Result calculate -: " + equation);
+      return equation;
+
     case "X":
-      firstOperand = Number(firstNumber);
-      secondOperand = Number(secondNumber);
-      result = firstOperand * secondOperand;
-      console.log("Result: " + result);
-      break;
+      firstNumber = Number(firstOperand);
+      secondNumber = Number(secondOperand);
+      result = firstNumber * secondNumber;
+      equation = `${firstOperand}${operator}${secondOperand}=${result} `;
+      console.log("Result calculate *: " + equation);
+      return equation;
+
     case "/":
-      firstOperand = Number(firstNumber);
-      secondOperand = Number(secondNumber);
-      result = firstOperand / secondOperand;
-      console.log("Result: " + result);
-      break;
+      firstNumber = Number(firstOperand);
+      secondNumber = Number(secondOperand);
+      result = firstNumber / secondNumber;
+      equation = `${firstOperand}${operator}${secondOperand}=${result} `;
+      console.log("Result calculate /: " + equation);
+      return equation;
     default:
       console.log(`Sorry, wrong operator: ${operator}.`);
   }
 };
-const ShowCalculation = () => {
-  console.log(`ShowCalculation ${firstNumber}${operator}${secondNumber}=`);
-};
 
-const isOperator = (buttonValue) => {
-  return (
-    buttonValue === "+" ||
-    buttonValue === "-" ||
-    buttonValue === "X" ||
-    buttonValue === "/"
-  );
-};
-
-export default calculateNumbers;
+export default calculateResult;
