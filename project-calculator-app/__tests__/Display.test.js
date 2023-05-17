9245acc4b8e1398b2b468d9b4485a6c8f6dbb62b
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-17 14:41:29 +1200

    adding operator and error tests

--- project-calculator-app/__tests__/Display.test.js
+++ project-calculator-app/__tests__/Display.test.js
@@ -24,6 +24,15 @@ describe("Display component", () => {
     fireEvent.press(getByText("="));
     expect(getByText("5+3=8")).toBeTruthy();
   });
+  test("should perform a calculation on * not + operator", () => {
+    const { getByText } = render(<Display />);
+    fireEvent.press(getByText("5"));
+    fireEvent.press(getByText("+"));
+    fireEvent.press(getByText("3"));
+    fireEvent.press(getByText("X"));
+    fireEvent.press(getByText("="));
+    expect(getByText("5X3=15")).toBeTruthy();
+  });
   test("should handle division by zero", () => {
     const { getByText } = render(<Display />);
     fireEvent.press(getByText("8"));
@@ -31,5 +40,22 @@ describe("Display component", () => {
     fireEvent.press(getByText("0"));
     fireEvent.press(getByText("="));
     expect(getByText("Not devisable by 0")).toBeTruthy();
+  });
+  test("should not show first = in equation", () => {
+    const { getByText } = render(<Display />);
+    fireEvent.press(getByText("="));
+    fireEvent.press(getByText("5"));
+    fireEvent.press(getByText("+"));
+    fireEvent.press(getByText("3"));
+    fireEvent.press(getByText("="));
+    expect(getByText("5+3=8")).toBeTruthy();
+  });
+  test("should not show first = in equation", () => {
+    const { getByText } = render(<Display />);
+    fireEvent.press(getByText("."));
+    fireEvent.press(getByText("+"));
+    fireEvent.press(getByText("9"));
+    fireEvent.press(getByText("="));
+    expect(getByText("Error: Invalid input")).toBeTruthy();
   });
 });
