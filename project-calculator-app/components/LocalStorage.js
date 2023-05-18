1b58cb063806974fd858504b6834fe1e0e0de0b3
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-19 10:24:41 +1200

    updating stored data

--- project-calculator-app/components/LocalStorage.js
+++ project-calculator-app/components/LocalStorage.js
@@ -1,23 +1,26 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const storeData = async (equation) => {
+export const storeData = async (equation) => {
   try {
-    const storedEquations = await getData();
-    const UpdatingEquations = [...storedEquations, equation];
-    console.log("storage:" + UpdatingEquations);
-    const jsonValue = JSON.stringify(UpdatingEquations);
+    console.log("storage: " + equation);
+    let storedEquations = await getData();
+
+    if (!storedEquations) {
+      storedEquations = [];
+    }
+    const updatingEquations = [...storedEquations, equation];
+
+    const jsonValue = JSON.stringify(updatingEquations);
     await AsyncStorage.setItem("@storage_Key", jsonValue);
-  } catch (e) {
-    // saving error
+  } catch (error) {
+    console.log("Error storing data:", error);
   }
 };
-const getData = async () => {
+export const getData = async () => {
   try {
     const jsonValue = await AsyncStorage.getItem("@storage_Key");
-    return jsonValue != null ? JSON.parse(jsonValue) : null;
-  } catch (e) {
-    // error reading value
+    return jsonValue != null ? JSON.parse(jsonValue) : [];
+  } catch (error) {
+    console.log("Error getting data:", error);
   }
 };
-
-export default storeData;
