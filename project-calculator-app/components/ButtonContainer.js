f0c789e63aa3459fccd0cc8d443f5912df487221
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-20 20:05:02 +1200

    adding a backspace arrow other than "C" for matching

--- project-calculator-app/components/ButtonContainer.js
+++ project-calculator-app/components/ButtonContainer.js
@@ -1,11 +1,23 @@
 import { Pressable, Text, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-const buttons = ({ value, rightValue, onButtonPress }) => {
+const Buttons = ({ value, rightValue, onButtonPress }) => {
   const operator =
     value === "/" ||
     rightValue === "+" ||
     rightValue === "-" ||
     rightValue === "X";
+
+  let buttonContent = null;
+  if (value === "C") {
+    buttonContent = <Feather name="arrow-left" size={32} color="white" />;
+  } else {
+    buttonContent = (
+      <Text style={[styles.value, value === "AC" && styles.allClearButton]}>
+        {value || rightValue}
+      </Text>
+    );
+  }
 
   return (
     <Pressable
@@ -20,19 +32,11 @@ const buttons = ({ value, rightValue, onButtonPress }) => {
       ]}
       onPress={onButtonPress}
     >
-      <Text
-        style={[
-          styles.value,
-          value === "AC" && styles.allClearButton,
-          value === "C" && styles.backspaceButton,
-        ]}
-      >
-        {value || rightValue}
-      </Text>
+      {buttonContent}
     </Pressable>
   );
 };
-export default buttons;
+export default Buttons;
 
 const styles = StyleSheet.create({
   buttons: {
