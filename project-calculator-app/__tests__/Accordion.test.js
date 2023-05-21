199458872166806608af789873557ba87936a76e
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-21 18:35:04 +1200

    removed comments

--- project-calculator-app/__tests__/Accordion.test.js
+++ project-calculator-app/__tests__/Accordion.test.js
@@ -8,24 +8,17 @@ describe("EquationListAccordion", () => {
       <Accordion storedData={storedData} />
     );
 
-    // Verify initial state
     expect(queryByText("2 + 2")).toBeNull();
     expect(queryByText("3 * 4")).toBeNull();
 
-    // Press the pressable component
     fireEvent.press(getByText("History"));
 
-    // Verify that the dropdown is open and displays the stored data
     expect(queryByText("2 + 2")).toBeTruthy();
     expect(queryByText("3 * 4")).toBeTruthy();
 
-    // Press the pressable component again to close the dropdown
     fireEvent.press(getByText("History"));
 
-    // Verify that the dropdown is closed
     expect(queryByText("2 + 2")).toBeNull();
     expect(queryByText("3 * 4")).toBeNull();
-
-    // Your other test cases...
   });
 });
